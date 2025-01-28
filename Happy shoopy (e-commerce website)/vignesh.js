bba2a2ec282351f5f44bc7eaa65ac066a866121a
Vignesh-0504 <vigneshsaravanan0504@gmail.com>
2025-01-28 11:08:31 +0530

    Add files via upload
    E-commerce website

---
+++ Happy shoopy (e-commerce website)/vignesh.js
@@ -0,0 +1,18 @@
+
+var sidenav = document.querySelector(".side-navbar")
+
+function show() {
+
+    sidenav.style.left = "0%"
+    // sidenav.style.display="block"
+}
+
+function hide() {
+
+    sidenav.style.left = "-50%"
+    // sidenav.style.display="none"
+}
+function popup() {
+    alert("Your account has been created successfully");
+};
+
